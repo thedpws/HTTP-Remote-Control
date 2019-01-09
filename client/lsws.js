6869a2e055789c1a5c8fd803c538e5dd02debb9c
Az Vasquez <azvasquez@Azs-MacBook-Air.local>
2019-01-09 13:58:04 -0500

    Remove console logging to speed up page

--- client/lsws.js
+++ client/lsws.js
@@ -1,7 +1,6 @@
 window.onload = sendPutTo('/update');
 
 function sendPutTo(pathname){
-  console.log(pathname);
   let xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = () => {
     if (xmlHttp.readyState === 4) updateInfo(xmlHttp.response);
@@ -11,7 +10,6 @@ function sendPutTo(pathname){
 }
 
 function updateInfo(obj){
-  console.log(obj);
   let json = JSON.parse(obj);
   document.getElementById("system-volume").innerText = json.systemVolume;
 }
