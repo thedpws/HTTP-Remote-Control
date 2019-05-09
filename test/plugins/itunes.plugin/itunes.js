03b18d90d273ddc344df40d7bfd081ad57b821c7
Az Vasquez <azvasquez99@gmail.com>
2019-05-09 19:01:34 -0400

    iTunes commands working

--- test/plugins/itunes.plugin/itunes.js
+++ test/plugins/itunes.plugin/itunes.js
@@ -1,45 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const shell = require('shelljs');
+const execute = (script) => {
+    shell.exec(`osascript ./plugins/itunes.plugin/${script}`);
+}
 
 
 router.post('/play', (req, res) => {
     // send play to itunes
     console.log('itunes play');
+    execute('play.scpt');
     res.sendStatus(501);
 });
 
 router.post('/pause', (req, res) => {
     // send pause to itunes
     console.log('itunes pause');
+    execute('pause.scpt');
     res.sendStatus(501);
 });
 
 router.post('/next', (req, res) => {
     // send next to itunes
     console.log('itunes next');
+    execute('next.scpt');
     res.sendStatus(501);
 });
 
 router.post('/prev', (req, res) => {
     // send prev to itunes
     console.log('itunes prev');
+    execute('prev.scpt');
     res.sendStatus(501);
 });
 
 router.post('/dec', (req, res) => {
     // send dec to itunes
     console.log('itunes decrescendo');
+    execute('dec.scpt');
     res.sendStatus(501);
 });
 
 router.post('/volup', (req, res) => {
     // send volume up to itunes
     console.log('itunes volume up');
+    execute('volup.scpt');
     res.sendStatus(501);
 });
 router.post('/voldown', (req, res) => {
     // send volume down to itunes
     console.log('itunes volume down');
+    execute('voldown.scpt');
     res.sendStatus(501);
 });
 
