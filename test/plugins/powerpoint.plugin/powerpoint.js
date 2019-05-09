59e5b4c221b795f622cadac27914039ad39f2bba
Az Vasquez <azvasquez99@gmail.com>
2019-05-09 19:02:55 -0400

    powerpoint commands

--- test/plugins/powerpoint.plugin/powerpoint.js
+++ test/plugins/powerpoint.plugin/powerpoint.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
+const shell = require('shelljs');
+const execute = (script) => {
+    shell.exec(`osascript ./plugins/itunes.plugin/${script}`);
+}
 
 router.post('/nextslide', (req, res) => {
     // send play to powerpoint
     console.log('ppt next');
+    execute('next.scpt');
     res.sendStatus(501);
 });
 
 router.post('/prevslide', (req, res) => {
     // send pause to powerpoint
     console.log('ppt previous');
+    execute('prev.scpt');
     res.sendStatus(501);
 });
 
